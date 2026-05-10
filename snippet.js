88d12ed182d6a557f9bf6edea4a4d2066c577cdb
João Vítor <jjoaovitor7@outlook.com.br>
2026-05-10 18:24:15 -0300

    add 'snippet.js'.

---
+++ snippet.js
@@ -0,0 +1,9 @@
+document.querySelectorAll("*").forEach((el) => {
+  el.style = `pointer-events:auto;
+    filter:none;
+    backdrop-filter:none;
+    user-select:auto;
+    user-select:auto;
+    -webkit-user-select:auto;
+    cursor:auto;`;
+});
